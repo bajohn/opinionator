b110cd5fddbfbeb866ba44a1ed120b74a8855d7d
Brendan John <bjohn454@gmail.com>
2020-10-26 22:24:35 -0400

    typingdna frontend imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var TypingDNA:any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'opinionator';
+  constructor() {
+    var tdna  = new TypingDNA();
+    console.log(tdna)
+    //anytext pattern
+    var typingPattern = tdna.getTypingPattern({type:0, length:160});
+    console.log(typingPattern)
+    //sametext pattern
+    var typingPattern = tdna.getTypingPattern({type:1, text:"Hello5g21?*"});
+    //extended pattern
+    var typingPattern = tdna.getTypingPattern({type:2, text:"example@mail.com"});
+  }
 }
